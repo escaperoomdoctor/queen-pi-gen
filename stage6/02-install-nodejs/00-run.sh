dfff0f298317b9ea683b5e29bdbf79ccc9f513e1
Alex Chiga <rus.mich@ya.ru>
2025-12-06 13:22:31 +0300

    reopen terminal during node instalation

--- stage6/02-install-nodejs/00-run.sh
+++ stage6/02-install-nodejs/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "${ROOTFS_DIR}/home/pi/.nvm/nvm.sh"
+source ~/.bashrc
 nvm install 20
 nvm install 22
 nvm install 24

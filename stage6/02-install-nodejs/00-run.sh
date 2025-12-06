4e883a22872f9713298a3964a43904e6bcf9ce22
Alex Chiga <rus.mich@ya.ru>
2025-12-06 12:56:25 +0300

    temporarily delete stage6

--- stage6/02-install-nodejs/00-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "${ROOTFS_DIR}/home/pi/.nvm/nvm.sh"
-nvm install 20
-nvm install 22
-nvm install 24

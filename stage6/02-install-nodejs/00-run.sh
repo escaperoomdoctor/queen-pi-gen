f5ae0f9dd7d002e37fdbe528049326702fe48a20
Alex Chiga <rus.mich@ya.ru>
2025-12-06 14:29:42 +0300

    add export NVM_DIR

--- stage6/02-install-nodejs/00-run.sh
+++ stage6/02-install-nodejs/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-
 on_chroot <<- EOF
+  export NVM_DIR="/home/pi/.nvm"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
   source ~/.bashrc
   nvm install 20

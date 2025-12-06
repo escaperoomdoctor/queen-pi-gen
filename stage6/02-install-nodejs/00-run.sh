7ba72b1c5e56a440174a7b7fb2f6d1c69e23c3bc
Alex Chiga <rus.mich@ya.ru>
2025-12-06 14:08:57 +0300

    run install nvm commands inside image

--- stage6/02-install-nodejs/00-run.sh
+++ stage6/02-install-nodejs/00-run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source ~/.bashrc
-nvm install 20
-nvm install 22
-nvm install 24
+
+on_chroot <<- EOF
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  source ~/.bashrc
+  nvm install 20
+  nvm install 22
+  nvm install 24
+EOF

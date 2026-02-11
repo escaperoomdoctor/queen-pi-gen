e3ea37142c8a138e242874bb0876e3ec38e500f8
Alex Chiga <rus.mich@ya.ru>
2026-02-11 16:18:21 +0300

    fix docker install

--- stage6/03-install-docker/00-run.sh
+++ stage6/03-install-docker/00-run.sh
@@ -3,12 +3,13 @@
 on_chroot <<- EOF
   apt-get install ca-certificates curl
   install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
   echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sed -i 's/raspbian/debian/g' /etc/apt/sources.list.d/docker.list
   apt-get update
   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 EOF

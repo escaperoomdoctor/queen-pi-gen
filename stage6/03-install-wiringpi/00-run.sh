2dec1804d0cfaf7b43c80c53ce5e832cbeb900d4
Alex Chiga <rus.mich@ya.ru>
2026-03-30 23:19:03 +0300

    temporarily delete scripts without execute permissions

--- stage6/03-install-wiringpi/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-on_chroot <<- EOF
-  curl -Lo- https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_armhf.deb | dpkg -i -
-EOF
--- stage6/04-install-bass/00-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-on_chroot <<- EOF
-  wget https://www.un4seen.com/files/bass24-linux.zip
-  unzip -j bass24-linux.zip libs/armhf/libbass.so -d /usr/local/lib/
-  ldconfig
-  rm -rf bass24-linux.zip
-EOF

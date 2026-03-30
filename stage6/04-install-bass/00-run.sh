55f95515f49e59e1820bb7c9ac4327abfc1c6fa6
Alex Chiga <rus.mich@ya.ru>
2026-03-30 23:38:18 +0300

    fix install bass

--- stage6/04-install-bass/00-run.sh
+++ stage6/04-install-bass/00-run.sh
@@ -4,5 +4,5 @@ on_chroot <<- EOF
   wget https://www.un4seen.com/files/bass24-linux.zip
   unzip -j bass24-linux.zip libs/armhf/libbass.so -d /usr/local/lib/
   ldconfig
-  rm -rf bass24-linux.zip
+  rm -f bass24-linux.zip
 EOF

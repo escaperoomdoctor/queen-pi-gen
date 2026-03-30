ced528de46e9b214ce2ab841acc481572b93e875
Alex Chiga <rus.mich@ya.ru>
2026-03-30 23:38:08 +0300

    fix install wiring pi

--- stage6/03-install-wiringpi/00-run.sh
+++ stage6/03-install-wiringpi/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
 on_chroot <<- EOF
-  curl -Lo- https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_armhf.deb | dpkg -i -
+  wget https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_armhf.deb
+  dpkg -i wiringpi_3.18_armhf.deb
+  rm -f wiringpi_3.18_armhf.deb
 EOF

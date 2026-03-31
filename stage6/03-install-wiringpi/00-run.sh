6f35d0dad1327ca3a22d21f8147ce5ef681f9090
Alex Chiga <rus.mich@ya.ru>
2026-03-31 14:15:32 +0300

    add sudo user for install wiringpi

--- stage6/03-install-wiringpi/00-run.sh
+++ stage6/03-install-wiringpi/00-run.sh
@@ -2,6 +2,6 @@
 
 on_chroot <<- EOF
   wget https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_armhf.deb
-  dpkg -i wiringpi_3.18_armhf.deb
+  SUDO_USER="${FIRST_USER_NAME}" dpkg -i wiringpi_3.18_armhf.deb
   rm -f wiringpi_3.18_armhf.deb
 EOF
